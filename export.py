7e8031d92b990e5d185935fb362ea000f942b8c2
Craftzman7 <zezetta@icloud.com>
2023-10-11 09:20:45 -0700

    update export script

--- export.py
+++ export.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('runs/detect/train/weights/best.pt')  # load a custom trained model
+num = input("What is the train folder numer? ")
+
+model = YOLO(f'runs/detect/train{num}/weights/best.pt')  # load a custom trained model
 
 # Export the model
 model.export(format='edgetpu')
