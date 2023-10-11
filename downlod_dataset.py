8af656642141878607c4903e4eb5c0deb1f6b391
Craftzman7 <zezetta@icloud.com>
2023-10-11 07:57:34 -0700

    first commit

---
+++ downlod_dataset.py
@@ -0,0 +1,4 @@
+from roboflow import Roboflow
+rf = Roboflow(api_key="YOUR_API_KEY")
+project = rf.workspace("3636").project("bunnybots-2023")
+dataset = project.version(3).download("yolov8", location="datasets")
---
+++ export.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+model = YOLO('runs/detect/train/weights/best.pt')  # load a custom trained model
+
+# Export the model
+model.export(format='edgetpu')
