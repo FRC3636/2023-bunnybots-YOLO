9fe5e34083a548da29a04ca881c6b15f95052a1c
Craftzman7 <zezetta@icloud.com>
2023-10-20 08:00:50 -0700

    a

--- downlod_dataset.py
+++ downlod_dataset.py
@@ -1,4 +1,4 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="YOUR_API_KEY")
 project = rf.workspace("3636").project("bunnybots-2023")
-dataset = project.version(3).download("yolov8", location="datasets")
+dataset = project.version(6).download("yolov8", location="datasets")
